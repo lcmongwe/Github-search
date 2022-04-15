7c17e688d1e833a902191bd8fabc34eaa019a2e4
lcmongwe <lcmongwe@gmail.com>
2022-04-15 21:38:01 +0300

    navbar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { GithubComponent } from './github/github.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    GithubComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
