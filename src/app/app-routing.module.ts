341c433f083eb07d51172dbe1f66da7a130c061d
lcmongwe <lcmongwe@gmail.com>
2022-04-17 07:52:34 +0300

    router form route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: FormComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: '**', component: NotFoundComponent },
+  // { path: 'goals/:id', component: GoalDetailComponent },
+  // { path: '', redirectTo: '/goals', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
