dccc5454d70bd585a03688cad7a7c4c91857aa8d
lcmongwe <lcmongwe@gmail.com>
2022-04-18 23:27:18 +0300

    repo service created

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RepoService {
+  constructor(private http: HttpClient) {}
+  async getRepo(repoName: any) {
+    const promise = this.http.get(`${environment.githuapi}/users/${repoName}`);
 
-  constructor() { }
+    return await lastValueFrom(promise)
+      .then((result) => result)
+      .catch((error) => console.log(error));
+  }
 }
