74fdcdb3f151705d42b4949ee40af4a4562e036a
lcmongwe <lcmongwe@gmail.com>
2022-04-20 06:27:09 +0300

    redirect path changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { GithubComponent } from './github/github.component';
 import { SearchComponent } from './search/search.component';
 import { FormComponent } from './form/form.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/git', pathMatch: 'full' },
+  { path: '', redirectTo: 'git', pathMatch: 'full' },
   { path: 'search', component: SearchComponent },
   { path: 'git', component: GithubComponent },
   { path: 'form', component: FormComponent },
